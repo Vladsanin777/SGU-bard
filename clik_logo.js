13563700761a28637de89511e7ed361e3563642b
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2024-01-01 08:33:46 +0400

    ftu

--- clik_logo.js
+++ clik_logo.js
@@ -14,9 +14,12 @@ function smenadizaina(){
     for ( let button of document.querySelectorAll(".button_5")){
         button.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
     }
-    document.querySelector(".new_univer").style.border = ['5px solid rgb(84, 194, 21)', '5px solid rgb(0, 0, 255)', '5px solid rgb(243, 219, 3)'][window.n_random]
-    document.querySelector(".new_univer_1").style.backgroundColor = ['rgb(84, 194, 21)', 'rgb(0, 0, 255)', 'rgb(243, 219, 3)'][window.n_random]
+    document.querySelector(".new_univer").style.backgroundColor = ['rgb(84, 194, 21)', 'rgb(0, 0, 255)', 'rgb(243, 219, 3)'][window.n_random];
+    document.querySelector(".new_univer_1").style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
     for ( let group of document.querySelectorAll(".new_univer_15")){
-        group.style.border = ['5px solid rgb(84, 194, 21)', '5px solid rgb(0, 0, 255)', '5px solid rgb(243, 219, 3)'][window.n_random];
+        group.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
+    }
+    for ( let text of document.querySelectorAll(".new_univer_1236")){
+        text.style.backgroundColor = ['rgb(84, 194, 21)', 'rgb(0, 0, 255)', 'rgb(243, 219, 3)'][window.n_random];
     }
 }
