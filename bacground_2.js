659621d3f5b0912480c1297b1b34d7e2aa92ccd4
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2024-01-01 05:16:14 +0400

    i

--- bacground_2.js
+++ bacground_2.js
@@ -1,3 +1,9 @@
+window.n_random = Math.floor(Math.random() * 3)
+// Установка цвета фона
+document.body.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
 //rgb(84, 194, 21)
 console.log(window.n_random)
 document.querySelector(".bacground_2").style.backgroundColor = ['rgb(84, 194, 21)', 'rgb(0, 0, 255)', 'rgb(243, 219, 3)'][window.n_random]
+for ( let button of document.querySelectorAll(".button_5")){
+    button.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
+}
--- clik_logo.js
+++ clik_logo.js
@@ -11,4 +11,7 @@ function smenadizaina(){
     }
     document.body.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
     document.querySelector(".bacground_2").style.backgroundColor = ['rgb(84, 194, 21)', 'rgb(0, 0, 255)', 'rgb(243, 219, 3)'][window.n_random]
+    for ( let button of document.querySelectorAll(".button_5")){
+        button.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
+    }
 }
