2c345c1f15012a5d3d70d03e052dbf3a8280a7b3
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2024-01-01 04:30:38 +0400

    l

--- clik_logo.js
+++ clik_logo.js
@@ -1,3 +1,14 @@
+function random_color(){
+    return Math.floor(Math.random() * 3)
+}
+
 function smenadizaina(){
-    
+    let random = random_color()
+    if (window.n_random == random){
+        random = random_color
+    } else {
+        window.n_random = random
+    }
+    document.body.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
+    document.querySelector(".bacground_2").style.backgroundColor = ['rgb(84, 194, 21)', 'rgb(0, 0, 255)', 'rgb(243, 219, 3)'][window.n_random]
 }
