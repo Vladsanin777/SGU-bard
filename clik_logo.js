9e8181830206bb98f174a8efaac0535be0a3d93d
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2024-01-02 01:56:17 +0400

    p

--- clik_logo.js
+++ clik_logo.js
@@ -2,6 +2,12 @@ function random_color(){
     return Math.floor(Math.random() * 3)
 }
 
+function ayrc_color(){
+    return ['rgb(80, 180, 35)', 'rgb(40, 50, 220', 'rgb(180, 180, 10)'][window.n_random];
+}
+function temn_color(){
+    return ['rgb(20, 60, 10)', 'rgb(20, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
+}
 function smenadizaina(){
     let random = random_color()
     if (window.n_random == random){
@@ -9,24 +15,24 @@ function smenadizaina(){
     } else {
         window.n_random = random
     }
-    document.body.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
-    document.querySelector(".bacground_2").style.backgroundColor = ['rgb(84, 194, 21)', 'rgb(0, 0, 255)', 'rgb(243, 219, 3)'][window.n_random]
+    document.body.style.backgroundColor = temn_color();
+    document.querySelector(".bacground_2").style.backgroundColor = ayrc_color();
     for ( let button of document.querySelectorAll(".button_5")){
-        button.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
+        button.style.backgroundColor = temn_color();
     }
-    document.querySelector(".new_univer").style.backgroundColor = ['rgb(84, 194, 21)', 'rgb(0, 0, 255)', 'rgb(243, 219, 3)'][window.n_random];
-    document.querySelector(".new_univer_1").style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
+    document.querySelector(".new_univer").style.backgroundColor = ayrc_color();
+    document.querySelector(".new_univer_1").style.backgroundColor = temn_color();
     for ( let group of document.querySelectorAll(".new_univer_15")){
-        group.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
+        group.style.backgroundColor = temn_color();
     }
     for ( let text of document.querySelectorAll(".new_univer_1236")){
-        text.style.backgroundColor = ['rgb(84, 194, 21)', 'rgb(0, 0, 255)', 'rgb(243, 219, 3)'][window.n_random];
+        text.style.backgroundColor = ayrc_color();
     }
     for ( let text of document.querySelectorAll(".new_univer_op")){
-        text.style.backgroundColor = ['rgb(84, 194, 21)', 'rgb(0, 0, 255)', 'rgb(243, 219, 3)'][window.n_random];
+        text.style.backgroundColor = ayrc_color();
     }
     for ( let group of document.querySelectorAll(".new_univer_op_b")){
-        group.style.backgroundColor = ['rgb(30, 65, 25)', 'rgb(25, 30, 65)', 'rgb(65, 63, 25)'][window.n_random];
+        group.style.backgroundColor = temn_color();
     }
 }
 smenadizaina()
