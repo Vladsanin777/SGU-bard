3a368e5ab253395b4d75876c3a685070c795fce4
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2024-02-13 03:58:01 +0400

    sd

---
+++ js/start_animation.js
@@ -0,0 +1,58 @@
+var duration = 15 * 1000;
+var animationEnd = Date.now() + duration;
+var skew = 1;
+
+function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+(function frame() {
+    var timeLeft = animationEnd - Date.now();
+    var ticks = Math.max(200, 500 * (timeLeft / duration));
+    skew = Math.max(0.8, skew - 0.001);
+
+    confetti({
+        particleCount: 1,
+        startVelocity: 0,
+        ticks: ticks,
+        origin: {
+            x: Math.random(),
+            // since particles fall down, skew start toward the top
+            y: (Math.random() * skew) - 0.2
+        },
+        colors: ['#ffffff'],
+        shapes: ['circle'],
+        gravity: randomInRange(0.4, 0.6),
+        scalar: randomInRange(0.4, 1),
+        drift: randomInRange(-0.4, 0.4)
+    });
+
+    if (timeLeft > 0) {
+        requestAnimationFrame(frame);
+    }
+}());
+
+var end;
+function frame_1() {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 }
+  });
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 }
+  });
+
+  if (Date.now() < end) {
+    requestAnimationFrame(frame_1);
+  }
+};
+document.querySelector('.SGU_zagolovok').addEventListener('click', () => {
+    console.log("f");
+    end = Date.now() + (15 * 1000)
+    frame_1();
+});
